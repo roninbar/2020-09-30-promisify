821d26698d12ab608a7f6f32e6155b2e10fe1f1b
ron <rinbar70@gmail.com>
2020-10-23 18:08:16 +0300

    Inline the bound functions.

--- script.js
+++ script.js
@@ -14,29 +14,26 @@ window.addEventListener('load', async function () {
             return this.op(a, b);
         },
     };
+
     const times = {
         op: (a, b) => a * b,
     };
-    const fooUnbound = plus.foo;
-    const fooBoundToTimes = plus.foo.bind(times);
-    const fooBound2ToTimes = plus.foo.bind2(times);
-    const fooBoundToTimes10 = plus.foo.bind(times, 10);
-    const fooBound2ToTimes10 = plus.foo.bind2(times, 10);
 
     const a = 5, b = 8;
 
     console.log('--- Testing bind2 ---');
     console.log(`plus.foo(${a}, ${b}) =>`, plus.foo(a, b)); // 13
     try {
+        const fooUnbound = plus.foo;
         console.log(`fooUnbound(${a}, ${b}) =>`);
         console.log(fooUnbound(a, b));
     } catch (err) {
         console.error(err); // TypeError: this.op is not a function
     }
-    console.log(`fooBoundToTimes(${a}, ${b}) =>`, fooBoundToTimes(a, b)); // 40
-    console.log(`fooBound2ToTimes(${a}, ${b}) =>`, fooBound2ToTimes(a, b)); // 40
-    console.log(`fooBoundToTimes10(${b}) =>`, fooBoundToTimes10(b)); // 80
-    console.log(`fooBound2ToTimes10(${b}) =>`, fooBound2ToTimes10(b)); // 80
+    console.log(`plus.foo.bind(times)(${a}, ${b}) =>`, plus.foo.bind(times)(a, b)); // 40
+    console.log(`plus.foo.bind2(times)(${a}, ${b}) =>`, plus.foo.bind2(times)(a, b)); // 40
+    console.log(`plus.foo.bind(times, 10)(${b}) =>`, plus.foo.bind(times, 10)(b)); // 80
+    console.log(`plus.foo.bind2(times, 10)(${b}) =>`, plus.foo.bind2(times, 10)(b)); // 80
     console.log('');
     
     console.log('--- Testing promisify ---');
